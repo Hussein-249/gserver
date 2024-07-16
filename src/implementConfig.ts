5196ad65daba085a76dbc18cbe79502f331d50bc
Hussein-249 <husseinadi8@gmail.com>
2024-07-16 12:45:26 -0400

    Minor UI work.

--- src/implementConfig.ts
+++ src/implementConfig.ts
@@ -1,7 +1,10 @@
 import * as fs from 'fs';
+import * as path from 'path';
 import simpleGit, { SimpleGit } from 'simple-git';
 
-const localPath = ' ';
+const localPath = path.join(__dirname, '');
+
+const envFilePath = '.env';
 
 function cloneRemoteRepository(remoteURL: string) {
     try{
@@ -19,6 +22,15 @@ function cloneRemoteRepository(remoteURL: string) {
 }
 
 
-function setEnv() {
+function setEnv(dbUsername: string, dbPassword: string, port: string, username: string, password: string, uploadKey: string) {
+    const DEV = `DEV_RESET=TRUE`;
+    const PORT = `PORT=${port}\n`;
+    const ARTICLE_COLL = `ARTICLE_COLL=Articles`;
+    const ANALYTICS_COLL = `ANALYTICS_COLL=Analytics`;
+    const LIVE_COLL = `LIVE_COLL=Live`;
+    const UPLOAD_KEY = `UPLOAD_KEY=${uploadKey}`;
+
+    // probably need to fix mongoDB role before continuing
+    // fs.writeFileSync
     return;
 }
